378bebdd1c4477811349d31d1d84d857a03ff1d6
Muruges <muruges95@hotmail.com>
2020-03-28 20:18:37 +0800

    added vanilla code for sudoku solver

--- sudoku/CS3243_P2_Sudoku_10.py
+++ sudoku/CS3243_P2_Sudoku_10.py
@@ -5,16 +5,101 @@ import copy
 # python file.py, ./path/to/init_state.txt ./output/output.txt
 
 class Sudoku(object):
-    def __init__(self, puzzle):
+    FAILURE = -1
+    def __init__(self, puzzle, domain_values=None):
         # you may add more attributes if you need
         self.puzzle = puzzle # self.puzzle is a list of lists
-        self.ans = copy.deepcopy(puzzle) # self.ans is a list of lists
+        self.ans = puzzle # self.ans is a list of lists
+        self.domain_values = domain_values if domain_values != None else []
+        if domain_values == None:
+            for i in range(9):
+                self.domain_values.append([])
+                for j in range(9):
+                    if puzzle[i][j] == 0:
+                        self.domain_values[i].append(range(1, 10))
+                    else:
+                        self.domain_values[i].append([puzzle[i][j]])
+
+    def get_unassigned(self):
+        return [(i,j) for i in range(9) for j in range(9) if self.puzzle[i][j] == 0]
 
     def solve(self):
-        # TODO: Write your code here
-        
+        unassigned = self.get_unassigned()
+        if len(unassigned) == 0:
+            return self.ans
+        var = self.select_unassigned_variable(unassigned)
+        var_row, var_col = var
+        ordered_domain_vals = self.get_and_order_domain_vals(var)
+        for val in ordered_domain_vals:
+            self.ans = copy.deepcopy(self.puzzle)
+            if self.assignment_is_consistent(var, val):
+                self.ans[var_row][var_col] = val
+                self.domain_values[var_row][var_col] = [val]
+                inf = self.inference()
+                if inf != self.FAILURE:
+                    new_sudoku = Sudoku(inf[0], domain_values=inf[1])
+                    result = new_sudoku.solve()
+                    if result != self.FAILURE:
+                        return result
+            # If we reach here we need to undo the assignment
+            self.ans[var_row][var_col] = 0
+            self.domain_values[var_row][var_col] = ordered_domain_vals
+
         # self.ans is a list of lists
-        return self.ans
+        return self.FAILURE
+
+    # We would not need to perform this if AC-3 is used at every step during inference and as preprocessing
+    def assignment_is_consistent(self, var, value):
+        # Need to perform 3 * 8 = 24 checks (within row, col and 3x3 square)
+        row, col = var
+        square_top_left_row = (row // 3) * 3
+        square_top_left_col = (col // 3) * 3
+        # Row check
+        for i in range(9):
+            if i == col:
+                continue
+            dom_values = self.domain_values[row][i]
+            # print(dom_values)
+            if len(dom_values) == 1 and dom_values[0] == value:
+                return False
+
+        # Col check
+        for i in range(9):
+            if i == row:
+                continue
+            dom_values = self.domain_values[i][col]
+            if len(dom_values) == 1 and dom_values[0] == value:
+                return False
+
+        # 3x3 square check
+        for i in range(square_top_left_row, square_top_left_row + 3):
+            for j in range(square_top_left_col, square_top_left_col + 3):
+                if (i, j) == (row, col):
+                    continue
+                dom_values = self.domain_values[i][j]
+                if len(dom_values) == 1 and dom_values[0] == value:
+                    return False
+        return True
+    
+    def inference(self):
+        # Do inference here such as AC-3
+        # Will need to make a deep copy of the domain values while doing inference
+        # so that we do not lose the information of the domain values of the
+        # other nodes. if we discover a variable for which we have an empty
+        # domain, we return FAILURE. Otherwise we can either return the original
+        # domains or the reduced domains
+        return (self.ans, self.domain_values)
+
+    
+    # We can choose a heuristic here for selecting an unassigned variable
+    def select_unassigned_variable(self, unassigned):
+        # For now just choose first variable
+        return unassigned[0]
+
+    def get_and_order_domain_vals(self, var):
+        domain_values = self.domain_values[var[0]][var[1]]
+        # We can add more stuff here for ordering the values
+        return domain_values # Currently no ordering
 
     # you may add more classes/functions if you think is useful
     # However, ensure all the classes/functions are in this file ONLY
@@ -48,9 +133,9 @@ if __name__ == "__main__":
 
     sudoku = Sudoku(puzzle)
     ans = sudoku.solve()
-
-    with open(sys.argv[2], 'a') as f:
-        for i in range(9):
-            for j in range(9):
-                f.write(str(ans[i][j]) + " ")
-            f.write("\n")
+    print(ans)
+    # with open(sys.argv[2], 'a') as f:
+    #     for i in range(9):
+    #         for j in range(9):
+    #             f.write(str(ans[i][j]) + " ")
+    #         f.write("\n")
